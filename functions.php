ceda613a606ff75fc724328972bad2ba0b156c46
Jonathan Denni <jonsdenni@gmail.com>
2014-01-31 17:51:32 -0800

    Add stylesheet, timthumb()

--- functions.php
+++ functions.php
@@ -6,8 +6,9 @@
 // register scripts and styles
 add_action('wp_enqueue_scripts', function(){
 	$js_dir = get_template_directory_uri() . '/js';
-
 	$min = ( ! wpedev_is_in_development() ) ? '.min' : '';
+
+	wp_register_style('proto2', get_template_directory_uri() . '/style.css' );
 });
 
 // register menus
@@ -26,4 +27,15 @@ if( ! function_exists('wpedev_is_in_development') ):
 		return strpos( get_bloginfo('url'), 'wpengine.com' ) !== false;
 	}
 endif;
+
+
+function timthumb( $src, $w=false, $h=false, $options=false){
+	//var_dump($src);
+	$rs = get_template_directory_uri() . '/timthumb.php';
+	$rs .= '?src=' . $src;
+	if( $w ){ $rs.= '&w=' . $w; }
+	if( $h ){ $rs.= '&h=' . $h; }
+	if( $options ){ $rs.= '&' . $options; }
+	return $rs;
+}
 ?>
