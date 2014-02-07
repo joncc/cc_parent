26992b6618a1b046b904a017bea0fafdc3a7a1ec
Jonathan Denni <jonsdenni@gmail.com>
2014-02-07 13:36:47 -0800

    Clean up header, show content in index.php

--- header.php
+++ header.php
@@ -24,44 +24,16 @@ function the_page_title(){
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-	<![endif]-->
 	<title><?php the_page_title() ?></title>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 	<header>
-		<?php //get_search_form(true); ?>
-		<!--nav id="navbar navbar-default navbar-static-top" role="navigation">
-		<?php wp_nav_menu(
-			array(
-				'theme_location' => 'account-menu',
-				'container_class' => 'nav navbar-nav'
-				)
-		); ?></nav-->
-	
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="navbar-header">
-				<button type="button"
-					class="navbar-toggle" data-toggle="collapse"
-					data-target="#main-menu"
-					>
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Brand</a>
-			</div>
+		<nav role="navigation">
 			<?php wp_nav_menu( array(
 				'theme_location' => 'main-menu',
-				'container_class' => 'collapse navbar-collapse',
 				'container_id' => 'main-menu',
-				'items_wrap'      => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
 			) ); ?>
 		</nav>
 	</header>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
-
+		<?php the_content() ?>
 	<?php endwhile; ?>
 <?php endif ?>
 
