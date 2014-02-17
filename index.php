818da0e270071657c8e028881e9ffc257d5e83ef
Jonathan Denni <jonsdenni@gmail.com>
2014-02-17 12:00:28 -0800

    Added <main> and no posts found text

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
 get_header(); ?>
-
+<main>
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php the_content() ?>
 	<?php endwhile; ?>
+<?php else: ?>
+	<p>No posts found</p>
 <?php endif ?>
-
+</main>
 <?php get_footer(); ?>
